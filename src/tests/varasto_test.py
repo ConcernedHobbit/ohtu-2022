76f3ce7c40927f0470b695de0a71ffba5e2c1fa1
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-03 04:50:15 +0200

    bump up varasto coverage to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -12,6 +12,12 @@ class TestVarasto(unittest.TestCase):
 
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+    
+    def test_virheellinen_tilavuus_nollautuu(self):
+        self.assertAlmostEqual(Varasto(-10).tilavuus, 0)
+
+    def test_virheellinen_alku_saldo_nollautuu(self):
+        self.assertAlmostEqual(Varasto(10, -10).saldo, 0)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
@@ -23,6 +29,16 @@ class TestVarasto(unittest.TestCase):
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_negatiivinen_lisays_ei_onnistu(self):
+        self.varasto.lisaa_varastoon(-8)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_ei_mene_yli_tilavuuden(self):
+        self.varasto.lisaa_varastoon(20)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +54,26 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_ottaminen_ei_onnistu(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_liika_ottaminen_palauttaa_kaiken(self):
+        self.varasto.lisaa_varastoon(8)
+
+        otettu = self.varasto.ota_varastosta(20)
+
+        self.assertAlmostEqual(otettu, 8)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_merkkijono_alussa(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+    def test_merkkijono_lisayksen_jalkeen(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertEqual(str(self.varasto), "saldo = 8, vielä tilaa 2")
