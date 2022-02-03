5d7433dd308ec8edf7e9bf99da005f5b822f2535
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-02-03 10:10:24 +0200

    break warehouse init (on purpose)

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,9 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        self.tilavuus = max(tilavuus, 0.0)
+        if tilavuus > 0.0:
+            self.tilavuus = tilavuus
+        else:
+            self.tilavuus = 0.0
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
